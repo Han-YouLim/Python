f1072dc13be6553c3ac3a90c63bc6d42c8af1586
Han-YouLim <gksdbfla12@naver.com>
2021-02-01 00:07:10 +0900

    Hello

---
+++ twonumadd.py
@@ -0,0 +1,15 @@
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+    # 키와 값을 바꿔서 딕셔너리로 저장
+    for i, num in enumerate(nums):
+        print(i,num,end="/  ")
+        nums_map[num] = i
+    print()
+    print(nums_map)
+    # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            return [i, nums_map[target - num]]
+
+print(twoSum([2,7,11,15,2], 9))
